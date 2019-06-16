7e3a837832c1fceecc63a87aaba663d95aa06333
Kelina Shrestha <kylesth@gmail.com>
2019-06-16 22:31:28 +0545

    Open in new tab for insta link.

--- components/Footer.vue
+++ components/Footer.vue
@@ -43,7 +43,7 @@
       </div>
       <div class="row">
         <div class="col-12 col-md text-center">
-          <a href="https://www.instagram.com/shizenluver/" class="social-icon">
+          <a href="https://www.instagram.com/shizenluver/" class="social-icon" target="_blank">
             <div class="icon-social-instagram text-danger"></div>
           </a>
           <small class="d-block text-muted mt-2">Copyright &copy; Kelina Shrestha</small>
